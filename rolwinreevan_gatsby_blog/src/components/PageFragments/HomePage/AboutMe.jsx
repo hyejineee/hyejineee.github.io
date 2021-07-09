c64720ab59a1979d8ed24159dd91b587b7d34785
hyejineee <dev.hyejin04@gmail.com>
2021-07-09 22:01:07 +0900

    Change about me test

--- rolwinreevan_gatsby_blog/src/components/PageFragments/HomePage/AboutMe.jsx
+++ rolwinreevan_gatsby_blog/src/components/PageFragments/HomePage/AboutMe.jsx
@@ -6,12 +6,7 @@ import { stripTags, domHtml } from '../../../utils/stripTags';
 import SEO from '../../Seo';
 
 const pageText = {
-  paraOne: `Hello !! My name is Rolwin Reevan Monteiro. I'm a full stack web developer who is
-    passionate about various web technologies. I like to experiment with different web
-    technologies. I have an experience of nearly 3 years working with LAMP stack, MERN stack
-    and ELK stack. Building fancy UI's just like this one that your seeing 😅 and writing blogs about tech stacks
-    is what Rolwin loves to do. Check my blog which I update every week for some Javascript and some
-    cool notes on web technologies.`,
+  paraOne: '안녕하세요! 개발자 현혜진입니다!',
   paraTwo: `Currently I work mostly with Javascript technologies like ReactJS and NodeJS. I also
     have hands on experience working with cloud infrastructures like <b>AWS/GCP</b> and have deployed applications
     keeping scalability in mind. Docker, Kubernetes, Jenkins, SonarQube are some of the cool
